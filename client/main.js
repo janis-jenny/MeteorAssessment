5ee0152b81ff4aadcf3c88c709322f8d5eaf5d93
janis-jenny <pcardenasvm@gmail.com>
2021-08-20 23:47:17 -0500

    Delete imports folder and add lib folder with a collection.js to create a comments collection

--- client/main.js
+++ client/main.js
@@ -1,18 +1,10 @@
-/* import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import { App } from '/imports/ui/App';
-import '../imports/ui/App.js';
-
-Meteor.startup(() => {
-  render(<App/>, document.getElementById('react-target'));
-}); */
+import { Template } from 'meteor/templating';
 import './main.html';
 
 Template.mainContainer.helpers({
   comments: [
-    { text: 'This is task 1' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
+    { text: 'This is comment 1' },
+    { text: 'This is comment 2' },
+    { text: 'This is comment 3' },
   ],
 });
