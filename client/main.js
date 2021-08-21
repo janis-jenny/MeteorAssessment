176b3ada2361a4054f1dcfb6f97850f3e34fe00f
janis-jenny <pcardenasvm@gmail.com>
2021-08-21 00:19:26 -0500

    Insert hard code comments on the server side and call them to client side

--- client/main.js
+++ client/main.js
@@ -1,10 +1,14 @@
 import { Template } from 'meteor/templating';
+import { CommentsCollection } from '../lib/collection';
 import './main.html';
 
 Template.mainContainer.helpers({
-  comments: [
+  /* comments: [
     { text: 'This is comment 1' },
     { text: 'This is comment 2' },
     { text: 'This is comment 3' },
-  ],
+  ], */
+  comments() {
+    return CommentsCollection.find({});
+  }
 });
--- server/main.js
+++ server/main.js
@@ -1,32 +1,14 @@
-/* import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import { Meteor } from 'meteor/meteor';
+import { CommentsCollection } from '../lib/collection';
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
-}
-
+const insertComment = commentText => CommentsCollection.insert({ text: commentText });
+ 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
-
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
+  if (CommentsCollection.find().count() === 0) {
+    [
+      'First Comment',
+      'Second Comment',
+      'Third Comment',
+    ].forEach(insertComment)
   }
 });
- */
