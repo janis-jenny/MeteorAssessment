d05c4e978952f0b7657fbd1e205f73a259489d8d
janis-jenny <pcardenasvm@gmail.com>
2021-08-20 23:28:45 -0500

    Add main container template to render comments

--- client/main.js
+++ client/main.js
@@ -8,3 +8,11 @@ Meteor.startup(() => {
   render(<App/>, document.getElementById('react-target'));
 }); */
 import './main.html';
+
+Template.mainContainer.helpers({
+  comments: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+});
