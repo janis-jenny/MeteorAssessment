8609902f7eb3042b1a79a069cae8616b2620cb49
janis-jenny <pcardenasvm@gmail.com>
2021-08-21 20:50:35 -0500

    Add Navbar and style

--- client/main.js
+++ client/main.js
@@ -3,11 +3,6 @@ import { CommentsCollection } from '../lib/collection';
 import './main.html';
 
 Template.mainContainer.helpers({
-  /* comments: [
-    { text: 'This is comment 1' },
-    { text: 'This is comment 2' },
-    { text: 'This is comment 3' },
-  ], */
   comments() {
     return CommentsCollection.find({});
   }
